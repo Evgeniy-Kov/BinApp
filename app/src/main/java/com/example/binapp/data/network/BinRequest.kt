99a1b5d582fd381a54f306280fffb6e96eb70477
Evgeniy <none>
2025-01-11 23:51:51 +0300

    Add request and response classes

---
+++ app/src/main/java/com/example/binapp/data/network/BinRequest.kt
@@ -0,0 +1,3 @@
+package com.example.binapp.data.network
+
+data class BinRequest(val expression: String)
---
+++ app/src/main/java/com/example/binapp/data/network/BinResponse.kt
@@ -0,0 +1,5 @@
+package com.example.binapp.data.network
+
+import com.example.binapp.data.dto.BinInfoDto
+
+class BinResponse(val result: BinInfoDto) : Response()
