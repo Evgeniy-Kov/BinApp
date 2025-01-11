7d58d55e75963e3cfa2e3450e87e53a43faeda35
Evgeniy <none>
2025-01-12 00:41:21 +0300

    Add ViewBinding

--- app/src/main/java/com/example/binapp/ui/root/RootActivity.kt
+++ app/src/main/java/com/example/binapp/ui/root/RootActivity.kt
@@ -1,17 +1,23 @@
-package com.example.binapp
+package com.example.binapp.ui.root
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.binapp.databinding.ActivityRootBinding
 
-class MainActivity : AppCompatActivity() {
+class RootActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityRootBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
